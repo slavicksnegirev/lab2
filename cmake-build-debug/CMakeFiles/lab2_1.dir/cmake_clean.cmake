4428913dfc6d90311056a985b4dc233c981434e8
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2023-03-05 11:05:35 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/lab2_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2_1.dir/main.c.o"
+  "CMakeFiles/lab2_1.dir/main.c.o.d"
+  "lab2_1"
+  "lab2_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
